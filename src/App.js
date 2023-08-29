d045c393b3df7ebb2d4e784c1e5b02d25382e8fd
Marlon Taborda <taborda.marlon@gmail.com>
2023-08-29 15:47:19 -0300

    New pages and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { useState } from 'react';
+import Home from './pages/Home';
+import { Route } from 'react-router-dom';
+import Router from './router';
 
 function App() {
+  const [count, setCount] = useState(0);
+function increment(){
+  setCount(count+1);
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router/>
   );
 }
 
